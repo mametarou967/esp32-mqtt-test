eb53f6605a922b7be64139be68c4ee53896e62d8
mametarou967 <mametarou967@gmail.com>
2022-06-14 23:15:18 +0900

    mqttのfreeRTOSでの実行の確認完了

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # esp32-mqtt-test
+
+FreeRTOS版のmqttのコールバックは正しく動かないので、調査が必要
+以下のページにFreeRTOSでうまくいっている人がいる模様
+
+* https://forum.arduino.cc/t/need-some-help-setting-up-mqtt-on-esp32/948271/2
---
+++ esp32-freeRTOS-mqtt-task1-test/esp32-freeRTOS-mqtt-task1-test.ino
@@ -0,0 +1,68 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "freertos/FreeRTOS.h" //freeRTOS items to be used
+#include "wi-fi-config.h"
+
+const char* ssid     = MY_SSID;
+const char* password = MY_PASSWORD;
+const char *mqtt_broker = MY_BROKER;
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+void callback(char *topic, byte *payload, unsigned int length) {
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    for (int i = 0; i < length; i++) {
+        Serial.print((char) payload[i]);
+    }
+    Serial.println();
+    Serial.println("-----------------------");
+}
+
+void mqttTask( void *pvParameters ) {
+    // connecting to a WiFi network
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to WiFi..");
+    }
+    Serial.println("Connected to the WiFi network");
+    //connecting to a mqtt broker
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
+    while (!client.connected()) {
+        Serial.println("Connecting to public emqx mqtt broker.....");
+        if (client.connect("esp8266-client")) {
+            Serial.println("Public emqx mqtt broker connected");
+        } else {
+            Serial.print("failed with state ");
+            Serial.print(client.state());
+            delay(2000);
+        }
+    }
+    // publish and subscribe
+    client.subscribe("test");
+
+    for(;;)
+    {
+      client.loop();
+      vTaskDelay(1);
+    }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    xTaskCreateUniversal( mqttTask, "mqttTask", 15000, NULL, 6, NULL, 1 );
+}
+
+
+void loop() {
+  Serial.println("[main] hello");
+  vTaskDelay(3000);
+    
+}
